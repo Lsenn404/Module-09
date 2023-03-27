0a62117d0f09d3d518fe223520ef1649f84deb8f
Lsenn1 <lsenn@lion.lmu.edu>
2023-03-26 22:38:40 -0700

    added license badge with link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,28 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const MIT = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  const Apache = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  const GPL = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  const BSD = `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  const None = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+
+  if (license === "MIT") {
+    return MIT;
+  } else if (license === "Apache") {
+    return Apache;
+  } else if (license === "GPL") {
+    return GPL;
+  } else if (license === "BSD") {
+    return BSD;
+  } else if (license === "None") {
+    return None;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return renderLicenseBadge(license);
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-## Description  
-${data.description}
+  return `# ${data.title}  <br />
+${renderLicenseSection(data.license)}
 
-## Table of Contents
+## Table of Contents  <br />
+* [Description](#description)
 * [Installation](#installation)
 * [Usage](#usage)
 * [Contribution](#contribution)
 * [Test](#test)
 * [License](#license)
-* [Contact Me](#contact-me)
+* [Questions](#questions)
+
+## Description  
+${data.description}
 
 ## Installation
 ${data.installation}
@@ -36,11 +61,8 @@ ${data.contribution}
 ## Test 
 ${data.test}
 
-## License 
-${data.license}
-
 ## Questions
-Github: [${data.github}](https://github.com/${data.github})
+Github: [${data.github}](https://github.com/${data.github}) <br />
 Email: [${data.email}](mailto:${data.email})
 `;
 }
